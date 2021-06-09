8614522ec42b7d4176e31fa1a75a9f5b2b266d7e
am4n-raj <am4nraj@gmail.com>
2021-06-09 16:11:35 +0530

    Skills section added

--- script.js
+++ script.js
@@ -7,12 +7,24 @@ $(document).ready(function(){
         else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY>550){
+            $('.scroll-up-btn').addClass("show")
+        }
+        else{
+            $('.scroll-up-btn').removeClass("show")
+        }
     });
     // Toggle menu bar
     $('.menu-btn, .navbar .menu li a, .navbar .menuside').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
+    //scroll up btn function
+
+  $('.scroll-up-btn').click(function(){
+      $('html').animate({scrollTop:0});
+  });
 
 
 })
